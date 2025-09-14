a361a3403450faf0410e7fda5f862236806b9586
Maanikya <maanikya.acharya@gmail.com>
2025-09-15 01:35:56 +0530

    Reduced gaping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Base64 from "./components/base64";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center pb-18 gap-16">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center pb-18 gap-6">
       <Base64 />
     </div>
   );
