094dc79715909610164d2a86f5bf2943aad707ab
Maanikya <maanikya.acharya@gmail.com>
2025-09-15 01:57:56 +0530

    Base64 V3

--- src/app/components/base64.tsx
+++ src/app/components/base64.tsx
@@ -77,7 +77,7 @@ export default function Base64() {
   }, [text]);
 
   return (
-    <div className="absolute w-full flex flex-col items-center p-6">
+    <div id="main" className="absolute w-full flex flex-col items-center p-6">
 
       {/* Floating Blob */}
       <div
@@ -85,18 +85,19 @@ export default function Base64() {
         className="w-full h-[300px] bg-purple-600 opacity-20 rounded-full blur-3xl -z-10"
       ></div>
 
-      <h1 className="text-5xl mt-2 mb-4 text-white drop-shadow-lg">
-        Real-time Base64
+      <h1 id="heading" className="text-5xl mt-2 mb-6 text-white drop-shadow-lg">
+        Base64 Encrypt & Decrypt
       </h1>
 
       <textarea
+        id="textarea-input"
         className="w-[500px] h-26 bg-black/40 border border-white/30 p-4 rounded-lg text-lg resize-none focus:outline-none focus:ring-2 focus:ring-purple-500 transition-all text-white"
         placeholder="Type text or Base64 here..."
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
 
-      <div className="mt-6 space-y-6 w-[500px]">
+      <div id="text-output" className="mt-6 space-y-6 w-[500px]">
         {encoded && (
           <div className="flex flex-wrap gap-2 items-start">
             <strong
@@ -114,12 +115,18 @@ export default function Base64() {
         )}
 
         {decoded && (
-          <div
-            id="decode-box"
-            className="p-4 bg-white/10 border border-blue-500 rounded-lg break-words text-blue-300"
-          >
-            <strong>Decoded:</strong>
-            <p id="decode-text">{decoded}</p>
+          <div className="flex flex-wrap gap-2 items-start">
+            <strong
+              className="p-4 bg-white/10 border-blue-500 rounded-lg text-blue-300 whitespace-nowrap"
+            >
+              Decoded:
+            </strong>
+            <div
+              id="decode-box"
+              className="p-4 bg-white/10 border border-blue-500 rounded-lg break-words text-blue-300"
+            >
+              <p id="decode-text">{decoded}</p>
+            </div>
           </div>
         )}
       </div>
